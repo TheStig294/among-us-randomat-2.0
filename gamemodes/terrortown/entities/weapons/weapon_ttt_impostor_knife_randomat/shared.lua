f337e4a182c6b6a3ff8e1ee95104419a1b9b6504
thestig294 <thestig294@gmail.com>
2026-08-06 21:40:29 +0930

    Fixed weapons setting SWEP.WeaponID causing weapon names to be displayed improperly

--- gamemodes/terrortown/entities/weapons/weapon_ttt_impostor_knife_randomat/shared.lua
+++ gamemodes/terrortown/entities/weapons/weapon_ttt_impostor_knife_randomat/shared.lua
@@ -33,7 +33,6 @@ SWEP.Secondary.Automatic = true
 SWEP.Secondary.Ammo = "none"
 SWEP.Secondary.Delay = 1.4
 SWEP.Kind = WEAPON_EQUIP
-SWEP.WeaponID = AMMO_KNIFE
 SWEP.AllowDrop = false
 SWEP.IsSilent = true
 -- Pull out faster than standard guns
@@ -45,7 +44,7 @@ function SWEP:PrimaryAttack()
     if not IsValid(self:GetOwner()) then return end
     self:GetOwner():LagCompensation(true)
     local spos = self:GetOwner():GetShootPos()
-    local sdest = spos + (self:GetOwner():GetAimVector() * 70)
+    local sdest = spos + self:GetOwner():GetAimVector() * 70
     local kmins = Vector(1, 1, 1) * -10
     local kmaxs = Vector(1, 1, 1) * 10
 
@@ -95,7 +94,7 @@ function SWEP:PrimaryAttack()
         -- account we do want to avoid rounding error strangeness caused by
         -- other damage scaling, causing a death when we don't expect one, so
         -- when the target's health is close to kill-point we just kill
-        if hitEnt:Health() < (self.Primary.Damage + 10) then
+        if hitEnt:Health() < self.Primary.Damage + 10 then
             self:StabKill(tr, spos, sdest)
         else
             local dmg = DamageInfo()
@@ -105,7 +104,7 @@ function SWEP:PrimaryAttack()
             dmg:SetDamageForce(self:GetOwner():GetAimVector() * 5)
             dmg:SetDamagePosition(self:GetOwner():GetPos())
             dmg:SetDamageType(DMG_SLASH)
-            hitEnt:DispatchTraceAttack(dmg, spos + (self:GetOwner():GetAimVector() * 3), sdest)
+            hitEnt:DispatchTraceAttack(dmg, spos + self:GetOwner():GetAimVector() * 3, sdest)
         end
     end
 
@@ -146,7 +145,7 @@ function SWEP:StabKill(tr, spos, sdest)
     end
 
     -- seems the spos and sdest are purely for effects/forces?
-    target:DispatchTraceAttack(dmg, spos + (self:GetOwner():GetAimVector() * 3), sdest)
+    target:DispatchTraceAttack(dmg, spos + self:GetOwner():GetAimVector() * 3, sdest)
     -- target appears to die right there, so we could theoretically get to
     -- the ragdoll in here...
     self:Remove()
@@ -156,8 +155,8 @@ function SWEP:SecondaryAttack()
 end
 
 function SWEP:Equip()
-    self:SetNextPrimaryFire(CurTime() + (self.Primary.Delay * 1.5))
-    self:SetNextSecondaryFire(CurTime() + (self.Secondary.Delay * 1.5))
+    self:SetNextPrimaryFire(CurTime() + self.Primary.Delay * 1.5)
+    self:SetNextSecondaryFire(CurTime() + self.Secondary.Delay * 1.5)
 end
 
 function SWEP:PreDrop()
@@ -177,7 +176,7 @@ if CLIENT then
     function SWEP:DrawHUD()
         local tr = self:GetOwner():GetEyeTrace(MASK_SHOT)
 
-        if tr.HitNonWorld and IsValid(tr.Entity) and tr.Entity:IsPlayer() and tr.Entity:Health() < (self.Primary.Damage + 10) then
+        if tr.HitNonWorld and IsValid(tr.Entity) and tr.Entity:IsPlayer() and tr.Entity:Health() < self.Primary.Damage + 10 then
             local x = ScrW() / 2.0
             local y = ScrH() / 2.0
             surface.SetDrawColor(255, 0, 0, 255)
